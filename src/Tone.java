ccdec8cbcb534196e513bf9e32ada790b7ec5148
Cody Skala <cws26@zips.uakron.edu>
2016-04-26 00:51:33 -0400

    Sound now scales depending on the value of the note. Now we can try implementing it fo reel

--- src/Tone.java
+++ src/Tone.java
@@ -1,52 +1,82 @@
-//package sortingsolution;
+package tone;
 
 import javax.sound.sampled.*;
 
 public class Tone {
+    public static void main(String[] args) throws LineUnavailableException {
+        final AudioFormat af = new AudioFormat(16*1024, 8, 1, true, true);
+        SourceDataLine line = AudioSystem.getSourceDataLine(af);
+        line.open(af, 16*1024);
+        line.start();
+        int[] vals = new int[100];
+        Integer min = null;
+        Integer max = null;
+        for(int i = 1; i <= 100; ++i){
+            vals[i-1] = i%5 * i;
+            if(min == null || vals[i-1] < min){
+                min = vals[i-1];
+            }
+            if(max == null || vals[i-1] > max){
+                max = vals[i-1];
+            }
+        }
+        double scale = max - min + 1;
+        scale = scale / 13;
+        System.out.println(scale);
+        for(int i = 0; i < vals.length; ++i){
+            byte[] b = new byte[16*1024*2];
+            double exp = ((double) vals[i]) / 6d / scale;
+                double f = 440d * Math.pow(2d, exp);
+                for(int j = 0; j < b.length; j++){
+                    double period = (double)32*1024 / f;
+                    double angle = 2.0 * Math.PI * j / period;
+                    b[j] = (byte)(Math.sin(angle) * 127f);
+                }
+            line.write(b, 0, 600);
+            b = new byte[16*1024*2];
+            line.write(b, 0, 150);
+        }
+        line.drain();
+        line.close();
+    }
+}
 
-    /*public static void main(String[] args) throws LineUnavailableException {
+/*public class Tone {
+
+    public static void main(String[] args) throws LineUnavailableException {
         final AudioFormat af =
             new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, true);
         SourceDataLine line = AudioSystem.getSourceDataLine(af);
         line.open(af, Note.SAMPLE_RATE);
         line.start();
-        for(Note n : Note.values()){
-            play(line, n, 20);
+        for  (Note n : Note.values()) {
+            play(line, n, 500);
             play(line, Note.REST, 10);
         }
         line.drain();
         line.close();
-    }*/
+    }
 
-    private static void play(SourceDataLine line, Note note, int ms){
+    private static void play(SourceDataLine line, Note note, int ms) {
         ms = Math.min(ms, Note.SECONDS * 1000);
         int length = Note.SAMPLE_RATE * ms / 1000;
-        line.write(note.data(), 0, length);
+        int count = line.write(note.data(), 0, length);
     }
 }
 
 enum Note {
 
-    REST,
-    A0, A0$, B0, C0, C0$, D0, D0$, E0, F0, F0$, G0, G0$,
-    A1, A1$, B1, C1, C1$, D1, D1$, E1, F1, F1$, G1, G1$,
-    A2, A2$, B2, C2, C2$, D2, D2$, E2, F2, F2$, G2, G2$,
-    A3, A3$, B3, C3, C3$, D3, D3$, E3, F3, F3$, G3, G3$,
-    A4, A4$, B4, C4, C4$, D4, D4$, E4, F4, F4$, G4, G4$,
-    A5, A5$, B5, C5, C5$, D5, D5$, E5, F5, F5$, G5, G5$,
-    A6, A6$, B6, C6, C6$, D6, D6$, E6, F6, F6$, G6, G6$,
-    A7, A7$, B7, C7, C7$, D7, D7$, E7, F7, F7$, G7, G7$,
-    A8, A8$, B8, C8;
+    REST, A4, A4$, B4, C4, C4$, D4, D4$, E4, F4, F4$, G4, G4$, A5;
     public static final int SAMPLE_RATE = 16 * 1024; // ~16KHz
     public static final int SECONDS = 2;
-    private final byte[] sin = new byte[SECONDS * SAMPLE_RATE];
+    private byte[] sin = new byte[SECONDS * SAMPLE_RATE];
 
-    Note(){
+    Note() {
         int n = this.ordinal();
-        if (n > 0){
-            double exp = ((double) n - 1) / 16d;
-            double f = 110d * Math.pow(1.6d, exp);
-            for(int i = 0; i < sin.length; i++){
+        if (n > 0) {
+            double exp = ((double) n - 1) / 12d;
+            double f = 440d * Math.pow(2d, exp);
+            for (int i = 0; i < sin.length; i++) {
                 double period = (double)SAMPLE_RATE / f;
                 double angle = 2.0 * Math.PI * i / period;
                 sin[i] = (byte)(Math.sin(angle) * 127f);
@@ -57,4 +87,4 @@ enum Note {
     public byte[] data() {
         return sin;
     }
-}
+}*/

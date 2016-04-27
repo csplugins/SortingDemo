ecd416587d4c6e6750c7e428dc3ef889238a9744
Cody Skala <cws26@zips.uakron.edu>
2016-04-27 17:29:14 -0400

    Audio can now play two notes simultaneously

--- src/Tone.java
+++ src/Tone.java
@@ -6,13 +6,16 @@ public class Tone {
     public static void main(String[] args) throws LineUnavailableException {
         final AudioFormat af = new AudioFormat(16*1024, 8, 1, true, true);
         SourceDataLine line = AudioSystem.getSourceDataLine(af);
+        SourceDataLine line2 = AudioSystem.getSourceDataLine(af);
+        line2.open(af, 16*1024);
+        line2.start();
         line.open(af, 16*1024);
         line.start();
         int[] vals = new int[100];
         Integer min = null;
         Integer max = null;
         for(int i = 1; i <= 100; ++i){
-            vals[i-1] = i%5 * i;
+            vals[i-1] = 2*i;
             if(min == null || vals[i-1] < min){
                 min = vals[i-1];
             }
@@ -20,71 +23,30 @@ public class Tone {
                 max = vals[i-1];
             }
         }
-        double scale = max - min + 1;
-        scale = scale / 13;
-        System.out.println(scale);
-        for(int i = 0; i < vals.length; ++i){
+        double scale = (max - min + 1) / 13;
+        for(int i = 0; i < vals.length-20; ++i){
             byte[] b = new byte[16*1024*2];
-            double exp = ((double) vals[i]) / 6d / scale;
-                double f = 440d * Math.pow(2d, exp);
-                for(int j = 0; j < b.length; j++){
-                    double period = (double)32*1024 / f;
-                    double angle = 2.0 * Math.PI * j / period;
-                    b[j] = (byte)(Math.sin(angle) * 127f);
-                }
-            line.write(b, 0, 600);
-            b = new byte[16*1024*2];
+            byte[] b2 = new byte[16*1024*2];
             line.write(b, 0, 150);
+            line2.write(b2, 0, 150);
+            doMath(b, vals[i], scale);
+            doMath(b2, vals[i+20], scale);
+            line.write(b, 0, 1000);
+            line2.write(b2, 0, 1000);
         }
         line.drain();
         line.close();
+        line2.drain();
+        line2.close();
     }
-}
-
-/*public class Tone {
-
-    public static void main(String[] args) throws LineUnavailableException {
-        final AudioFormat af =
-            new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, true);
-        SourceDataLine line = AudioSystem.getSourceDataLine(af);
-        line.open(af, Note.SAMPLE_RATE);
-        line.start();
-        for  (Note n : Note.values()) {
-            play(line, n, 500);
-            play(line, Note.REST, 10);
-        }
-        line.drain();
-        line.close();
-    }
-
-    private static void play(SourceDataLine line, Note note, int ms) {
-        ms = Math.min(ms, Note.SECONDS * 1000);
-        int length = Note.SAMPLE_RATE * ms / 1000;
-        int count = line.write(note.data(), 0, length);
-    }
-}
-
-enum Note {
-
-    REST, A4, A4$, B4, C4, C4$, D4, D4$, E4, F4, F4$, G4, G4$, A5;
-    public static final int SAMPLE_RATE = 16 * 1024; // ~16KHz
-    public static final int SECONDS = 2;
-    private byte[] sin = new byte[SECONDS * SAMPLE_RATE];
-
-    Note() {
-        int n = this.ordinal();
-        if (n > 0) {
-            double exp = ((double) n - 1) / 12d;
-            double f = 440d * Math.pow(2d, exp);
-            for (int i = 0; i < sin.length; i++) {
-                double period = (double)SAMPLE_RATE / f;
-                double angle = 2.0 * Math.PI * i / period;
-                sin[i] = (byte)(Math.sin(angle) * 127f);
-            }
+    
+    private static void doMath(byte[] b, int val, double scale){
+        double exp = ((double) val) / 6d / scale;
+        double f = 440d * Math.pow(2d, exp);
+        for(int i = 0; i < b.length; i++){
+            double period = (double)32*1024 / f;
+            double angle = 2.0 * Math.PI * i / period;
+            b[i] = (byte)(Math.sin(angle) * 127f);
         }
     }
-
-    public byte[] data() {
-        return sin;
-    }
-}*/
+}

33cad28dc7530ef12b5e4b9d77ce41bf3dc24a7e
Joseph Haddad <jsh77@zips.uakron.edu>
2016-04-29 11:40:15 -0400

    de-indent

--- src/edu/akron/algorithms/sorts/RadixSort.java
+++ src/edu/akron/algorithms/sorts/RadixSort.java
@@ -11,35 +11,34 @@ public class RadixSort implements GenericSort {
     public Sorted sort(int[] elements) {
         final Queue<SortStep> q = new LinkedList<>();
         Queue<Integer> main_queue = new LinkedList<>();
-        if (elements.length > 0) {
-            int largest = elements[0];
-            for (int element : elements) {
-                main_queue.add(element);
-                largest = largest < element ? element : largest;
+        if (elements.length <= 0) return new Sorted(elements, q);
+        int largest = elements[0];
+        for (int element : elements) {
+            main_queue.add(element);
+            largest = largest < element ? element : largest;
+        }
+        int digits = 0;
+        do {
+            largest /= 10;
+            ++digits;
+        } while (largest != 0);
+        @SuppressWarnings("unchecked")
+        Queue<Integer>[] buckets = new Queue[10];
+        for (int i = 0; i < 10; ++i) {
+            buckets[i] = new LinkedList<>();
+        }
+        int den = 1;
+        for (int i = 0; i < digits; ++i) {
+            while (main_queue.size() > 0) {
+                int temp = main_queue.remove();
+                buckets[(temp / den) % 10].add(temp);
             }
-            int digits = 0;
-            do {
-                largest /= 10;
-                ++digits;
-            } while (largest != 0);
-            @SuppressWarnings("unchecked")
-            Queue<Integer>[] buckets = new Queue[10];
-            for (int i = 0; i < 10; ++i) {
-                buckets[i] = new LinkedList<>();
-            }
-            int den = 1;
-            for (int i = 0; i < digits; ++i) {
-                while (main_queue.size() > 0) {
-                    int temp = main_queue.remove();
-                    buckets[(temp / den) % 10].add(temp);
+            for (int j = 0; j < 10; ++j) {
+                while (buckets[j].size() > 0) {
+                    main_queue.add(buckets[j].remove());
                 }
-                for (int j = 0; j < 10; ++j) {
-                    while (buckets[j].size() > 0) {
-                        main_queue.add(buckets[j].remove());
-                    }
-                }
-                den *= 10;
             }
+            den *= 10;
         }
         int i = 0;
         while (main_queue.size() > 0) {

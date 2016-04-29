82be539ab33951a29faf65595d5c6a3f005e38bc
Joseph Haddad <jsh77@zips.uakron.edu>
2016-04-29 15:49:20 -0400

    Fix quick sort

--- src/edu/akron/algorithms/sorts/QuickSort.java
+++ src/edu/akron/algorithms/sorts/QuickSort.java
@@ -1,51 +1,56 @@
 package edu.akron.algorithms.sorts;
 
+import edu.akron.algorithms.visualize.Comparison;
 import edu.akron.algorithms.visualize.SortStep;
 import edu.akron.algorithms.visualize.Sorted;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class QuickSort implements GenericSort {
-    public int[] _sort(final Queue<SortStep> q, int[] elements, int right, int left) {
-        if (right - left + 1 < 2) return elements;
-        if (right - left + 1 == 2) {
-            if (elements[left] > elements[right]) {
-                int temp = elements[left];
-                elements[left] = elements[right];
-                elements[right] = temp;
+    public int[] _sort(final Queue<SortStep> q, int[] elements, int left, int right) {
+        int i = left, j = right;
+        final int m;
+        int pivot = elements[m = left + ((right - left) >> 1)];
+        while (i <= j) {
+            while (elements[i] < pivot) {
+                final int finalI = i;
+                q.offer(new SortStep(elements, new HashSet<Comparison>() {
+                    {
+                        add(Comparison.basic(finalI));
+                        add(Comparison.special(m));
+                    }
+                }));
+                ++i;
             }
-            return elements;
+            while (elements[j] > pivot) {
+                final int finalJ = j;
+                q.offer(new SortStep(elements, new HashSet<Comparison>() {
+                    {
+                        add(Comparison.basic(finalJ));
+                        add(Comparison.special(m));
+                    }
+                }));
+                --j;
+            }
+            if (i <= j) {
+                int temp = elements[i];
+                elements[i] = elements[j];
+                elements[j] = temp;
+                ++i;
+                --j;
+            }
         }
-        int a = elements[left], b = elements[(right - left) / 2], c = elements[right - left];
-        int pivot = ((a - b) * (b - c) > -1 ? (right - left) / 2 : ((a - b) * (a - c) < 1 ? 0 : right - left));
-        int temp = elements[pivot];
-        elements[pivot] = elements[right - left];
-        elements[right - left] = temp;
-        pivot = right - left;
-        int i = -1;
-        int j = right - left;
-        do {
-            while (elements[++i] < elements[pivot]) ;
-            while (i < j && elements[--j] > elements[pivot]) ;
-            temp = elements[i];
-            elements[i] = elements[j];
-            elements[j] = temp;
-        } while (i < j);
-        temp = elements[pivot];
-        elements[pivot] = elements[i];
-        elements[i] = temp;
-
-        _sort(q, elements, left, i - 1);
-        _sort(q, elements, left - right - i , right);
-
+        if (left < j) _sort(q, elements, left, j);
+        if (i < right) _sort(q, elements, i, right);
         return elements;
     }
 
     @Override
     public Sorted sort(int[] arr) {
         final Queue<SortStep> q = new LinkedList<>();
-        final int[] arr2 = _sort(q, arr, 0 , arr.length - 1);
+        final int[] arr2 = _sort(q, arr, 0, arr.length - 1);
         return new Sorted(arr2, q);
     }
 }

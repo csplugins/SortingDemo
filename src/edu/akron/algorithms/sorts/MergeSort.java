ee0d030451fb99c34c605f30e3141f18d1853560
Cody Skala <cws26@zips.uakron.edu>
2016-04-29 15:12:55 -0400

    Fixed merge as a recursive entire array

--- src/edu/akron/algorithms/sorts/MergeSort.java
+++ src/edu/akron/algorithms/sorts/MergeSort.java
@@ -7,50 +7,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class MergeSort implements GenericSort {
-    public int[] _sort(final Queue<SortStep> q, int[] elements) {
-        if (elements.length < 2)
+    public int[] _sort(final Queue<SortStep> q, int[] elements, int left , int right) {
+        if (right - left + 1 < 2)
             return elements;
-        if (elements.length == 2)
-            if (elements[0] > elements[1]) {
-                int temp = elements[0];
-                elements[0] = elements[1];
-                elements[1] = temp;
+        if (right - left + 1 == 2)
+            if (elements[left] > elements[right]) {
+                int temp = elements[left];
+                elements[left] = elements[right];
+                elements[right] = temp;
                 return elements;
             }
-        int halfPoint = elements.length / 2;
-        int[] firstHalf = new int[halfPoint];
-        int[] secondHalf = new int[elements.length - halfPoint];
-        System.arraycopy(elements, 0, firstHalf, 0, firstHalf.length);
-        System.arraycopy(elements, halfPoint, secondHalf, 0, secondHalf.length);
-        firstHalf = _sort(q, firstHalf);
-        secondHalf = _sort(q, secondHalf);
-        int i = 0, j = 0, count = 0;
-        while (i < firstHalf.length && j < secondHalf.length) {
-            if (firstHalf[i] <= secondHalf[j]) {
-                elements[count] = firstHalf[i];
-                ++i;
-            } else {
-                elements[count] = secondHalf[j];
-                ++j;
-            }
-            count++;
-        }
-        if (i < j)
-            for (int k = i; k < firstHalf.length; k++) {
-                elements[count] = firstHalf[k];
-                count++;
-            }
-        else for (int k = j; k < secondHalf.length; k++) {
-            elements[count] = secondHalf[k];
-            count++;
-        }
+        int halfPoint = (right - left + 1) / 2;
+        _sort(q, elements, left, halfPoint);
+        _sort(q, elements, halfPoint + 1 , right);
         return elements;
     }
 
     @Override
     public Sorted sort(int[] arr) {
         final Queue<SortStep> q = new LinkedList<>();
-        final int[] arr2 = _sort(q, arr);
+        final int[] arr2 = _sort(q, arr, 0, arr.length);
         return new Sorted(arr2, q);
     }
 }

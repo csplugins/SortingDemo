a94fd4cb740b9ee27fc2796b5f0878c668646193
Joseph Haddad <jsh77@zips.uakron.edu>
2016-04-29 11:22:07 -0400

    Add comparisons of bubble sort

--- src/edu/akron/algorithms/sorts/BubbleSort.java
+++ src/edu/akron/algorithms/sorts/BubbleSort.java
@@ -1,8 +1,10 @@
 package edu.akron.algorithms.sorts;
 
+import edu.akron.algorithms.visualize.Comparison;
 import edu.akron.algorithms.visualize.SortStep;
 import edu.akron.algorithms.visualize.Sorted;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -13,6 +15,13 @@ public class BubbleSort implements GenericSort {
         int n = elements.length;
         for (int i = 0; i < elements.length; ++i) {
             for (int j = 0; j < n - 1; ++j) {
+                final int finalJ = j;
+                q.offer(new SortStep(elements, new HashSet<Comparison>() {
+                    {
+                        add(Comparison.basic(finalJ));
+                        add(Comparison.basic(finalJ + 1));
+                    }
+                }));
                 if (elements[j] > elements[j + 1]) {
                     int temp = elements[j];
                     elements[j] = elements[j + 1];

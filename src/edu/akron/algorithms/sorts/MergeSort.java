b3554f91555672cae69bb70fe298c5881d2658c6
Joseph Haddad <jsh77@zips.uakron.edu>
2016-04-29 15:18:28 -0400

    Merge sort is done

--- src/edu/akron/algorithms/sorts/MergeSort.java
+++ src/edu/akron/algorithms/sorts/MergeSort.java
@@ -1,32 +1,46 @@
 package edu.akron.algorithms.sorts;
 
+import edu.akron.algorithms.visualize.Comparison;
 import edu.akron.algorithms.visualize.SortStep;
 import edu.akron.algorithms.visualize.Sorted;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class MergeSort implements GenericSort {
-    public int[] _sort(final Queue<SortStep> q, int[] elements, int left , int right) {
-        if (right - left + 1 < 2)
-            return elements;
-        if (right - left + 1 == 2)
-            if (elements[left] > elements[right]) {
-                int temp = elements[left];
-                elements[left] = elements[right];
-                elements[right] = temp;
-                return elements;
+    public int[] _sort(final Queue<SortStep> q, int[] elements, int left, int right) {
+        if (right - left < 1) return elements;
+        final int middle = (left + right) >> 1;
+        _sort(q, elements, left, middle);
+        _sort(q, elements, middle + 1, right);
+        int[] temp = new int[right - left + 1];
+        int k = 0, l = left, m = middle + 1;
+        while (l <= middle && m <= right) {
+            final int finalL = l;
+            final int finalM = m;
+            q.offer(new SortStep(elements, new HashSet<Comparison>() {
+                {
+                    add(Comparison.basic(finalL));
+                    add(Comparison.basic(finalM));
+                }
+            }));
+            if (elements[l] < elements[m]) {
+                temp[k++] = elements[l++];
+            } else {
+                temp[k++] = elements[m++];
             }
-        int halfPoint = (right - left + 1) / 2;
-        _sort(q, elements, left, halfPoint);
-        _sort(q, elements, halfPoint + 1 , right);
+        }
+        while (l <= middle) temp[k++] = elements[l++];
+        while (m <= right) temp[k++] = elements[m++];
+        for (int v = 0, i = left; i <= right; ++i, ++v) elements[i] = temp[v];
         return elements;
     }
 
     @Override
     public Sorted sort(int[] arr) {
         final Queue<SortStep> q = new LinkedList<>();
-        final int[] arr2 = _sort(q, arr, 0, arr.length);
+        final int[] arr2 = _sort(q, arr, 0, arr.length - 1);
         return new Sorted(arr2, q);
     }
 }

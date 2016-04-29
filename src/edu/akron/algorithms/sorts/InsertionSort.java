a0ba7b5560a64c48723b34c6cd83ce0816698603
Joseph Haddad <jsh77@zips.uakron.edu>
2016-04-29 11:22:18 -0400

    Add comparisons for insertion sort

--- src/edu/akron/algorithms/sorts/InsertionSort.java
+++ src/edu/akron/algorithms/sorts/InsertionSort.java
@@ -1,8 +1,11 @@
 package edu.akron.algorithms.sorts;
 
+import edu.akron.algorithms.visualize.Comparison;
 import edu.akron.algorithms.visualize.SortStep;
 import edu.akron.algorithms.visualize.Sorted;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -13,11 +16,20 @@ public class InsertionSort implements GenericSort {
         for (int i = 1; i < elements.length; i++) {
             int temp = elements[i];
             int placeToInsert = i;
-            while (placeToInsert > 0 && elements[placeToInsert - 1] > temp) {
+            while (placeToInsert > 0) {
+                final int finalPlaceToInsert = placeToInsert, finalI = i;
+                q.offer(new SortStep(elements, new HashSet<Comparison>() {
+                    {
+                        add(Comparison.basic(finalI));
+                        add(Comparison.basic(finalPlaceToInsert - 1));
+                    }
+                }));
+                if (elements[placeToInsert - 1] <= temp) continue;
                 elements[placeToInsert] = elements[placeToInsert - 1];
                 --placeToInsert;
             }
             elements[placeToInsert] = temp;
+            q.offer(new SortStep(elements, Collections.emptySet()));
         }
         return new Sorted(elements, q);
     }

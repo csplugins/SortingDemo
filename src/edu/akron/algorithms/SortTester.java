b2f259daf37b2d83108bbbc6a7020e60d9a576f4
Joseph Haddad <jsh77@zips.uakron.edu>
2016-04-30 15:56:39 -0400

    Add a sort tester

---
+++ src/edu/akron/algorithms/SortTester.java
@@ -0,0 +1,23 @@
+package edu.akron.algorithms;
+
+import edu.akron.algorithms.sorts.GenericSort;
+
+public class SortTester {
+    public static void main(final String[] params) {
+        for (final Sort sort : Sort.values()) {
+            final GenericSort s = sort.getSort();
+            for (final Sample sample : Sample.values()) {
+                final int[] a = sample.data();
+                final int[] validate = s.sort(a).array;
+                int prev = validate[0];
+                for (int i = 1; i < validate.length; ++i) {
+                    if (validate[i] < prev) {
+                        System.out.println(sort.toString() + " failed on " + sample.tag + ".");
+                        break;
+                    }
+                    prev = validate[i];
+                }
+            }
+        }
+    }
+}

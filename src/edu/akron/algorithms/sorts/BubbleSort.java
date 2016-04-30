c4fe9211d6886cf9f61233bc1827e79718bdbb76
Joseph Haddad <jsh77@zips.uakron.edu>
2016-04-30 15:11:30 -0400

    Remove tone references

--- src/edu/akron/algorithms/sorts/BubbleSort.java
+++ src/edu/akron/algorithms/sorts/BubbleSort.java
@@ -3,24 +3,15 @@ package edu.akron.algorithms.sorts;
 import edu.akron.algorithms.visualize.Comparison;
 import edu.akron.algorithms.visualize.SortStep;
 import edu.akron.algorithms.visualize.Sorted;
-import edu.akron.algorithms.tone.Tone;
 
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.sound.sampled.LineUnavailableException;
 
 public class BubbleSort implements GenericSort {
     @Override
     public Sorted sort(int[] elements) {
         final Queue<SortStep> q = new LinkedList<>();
-        Tone tone = null;
-        try {
-            tone = new Tone(elements);
-        } catch (LineUnavailableException ignored) {
-        }
         int n = elements.length;
         for (int i = 0; i < elements.length; ++i) {
             for (int j = 0; j < n - 1; ++j) {
@@ -31,7 +22,6 @@ public class BubbleSort implements GenericSort {
                         add(Comparison.basic(finalJ + 1));
                     }
                 }));
-                tone.PlayComparison(finalJ, finalJ + 1);
                 if (elements[j] > elements[j + 1]) {
                     int temp = elements[j];
                     elements[j] = elements[j + 1];
@@ -40,7 +30,6 @@ public class BubbleSort implements GenericSort {
             }
             n--;
         }
-        tone.PlaySorted(elements);
         return new Sorted(elements, q);
     }
 }
--- src/edu/akron/algorithms/sorts/MergeSort.java
+++ src/edu/akron/algorithms/sorts/MergeSort.java
@@ -1,6 +1,5 @@
 package edu.akron.algorithms.sorts;
 
-import edu.akron.algorithms.tone.Tone;
 import edu.akron.algorithms.visualize.Comparison;
 import edu.akron.algorithms.visualize.SortStep;
 import edu.akron.algorithms.visualize.Sorted;
@@ -8,14 +7,13 @@ import edu.akron.algorithms.visualize.Sorted;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-import javax.sound.sampled.LineUnavailableException;
 
 public class MergeSort implements GenericSort {
-    public int[] _sort(final Queue<SortStep> q, Tone tone, int[] elements, int left, int right) {
+    public int[] _sort(final Queue<SortStep> q, int[] elements, int left, int right) {
         if (right - left < 1) return elements;
         final int middle = (left + right) >> 1;
-        _sort(q, tone, elements, left, middle);
-        _sort(q, tone, elements, middle + 1, right);
+        _sort(q, elements, left, middle);
+        _sort(q, elements, middle + 1, right);
         int[] temp = new int[right - left + 1];
         int k = 0, l = left, m = middle + 1;
         while (l <= middle && m <= right) {
@@ -27,7 +25,6 @@ public class MergeSort implements GenericSort {
                     add(Comparison.basic(finalM));
                 }
             }));
-            tone.PlayComparison(finalL, finalM);
             if (elements[l] < elements[m]) {
                 temp[k++] = elements[l++];
             } else {
@@ -43,13 +40,7 @@ public class MergeSort implements GenericSort {
     @Override
     public Sorted sort(int[] arr) {
         final Queue<SortStep> q = new LinkedList<>();
-        Tone tone = null;
-        try {
-            tone = new Tone(arr);
-        } catch (LineUnavailableException ignored) {
-        }
-        final int[] arr2 = _sort(q, tone, arr, 0, arr.length - 1);
-        tone.PlaySorted(arr2);
+        final int[] arr2 = _sort(q, arr, 0, arr.length - 1);
         return new Sorted(arr2, q);
     }
 }

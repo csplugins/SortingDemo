af7a4ee6c6a330874a4bbd7aca70cd67cda5f0af
Cody Skala <cws26@zips.uakron.edu>
2016-05-01 20:41:10 -0400

    Brand new audio stuff

--- src/edu/akron/algorithms/tone/Tone.java
+++ src/edu/akron/algorithms/tone/Tone.java
@@ -1,56 +1,49 @@
 package edu.akron.algorithms.tone;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
+import javax.sound.midi.MidiUnavailableException;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
+import org.jfugue.realtime.RealtimePlayer;
+import org.jfugue.theory.Note;
 
 public class Tone {
-    final AudioFormat af;
-    SourceDataLine line;
-    SourceDataLine line2;
-    double scale;
+    RealtimePlayer player;
+    RealtimePlayer player2;
+    int scale;
+    int minVal;
 
-    public Tone(int[] elements) throws LineUnavailableException {
-        af = new AudioFormat(16 * 1024, 8, 1, true, true);
-        line = AudioSystem.getSourceDataLine(af);
-        line2 = AudioSystem.getSourceDataLine(af);
-        line2.open(af, 16 * 1024);
-        line2.start();
-        line.open(af, 16 * 1024);
-        line.start();
-        int minVal = Integer.MAX_VALUE, maxVal = Integer.MIN_VALUE;
+    public Tone(int[] elements) throws LineUnavailableException, MidiUnavailableException, InterruptedException {
+        //player.play("T220 m500/0.5 m525/0.5 m550/0.5 m575/0.5 m600/0.5 m625/0.5");
+        //new Player().play(new ChordProgression("I IV vi V").eachChordAs("$_i $_i Ri $_i"), new Rhythm().addLayer("..X...X...X...XO"));
+        player = new RealtimePlayer();
+        player2 = new RealtimePlayer();
+        minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
         for (final int element : elements) {
             if (element < minVal) minVal = element;
             if (element > maxVal) maxVal = element;
         }
-        scale = (maxVal - minVal + 1) / 14;
+        scale = maxVal - minVal;
     }
 
-    public void compare(final int duration, final int value1, final int value2) {
-        byte[] b = new byte[16 * 1024 * 2];
-        byte[] b2 = new byte[16 * 1024 * 2];
-        doMath(b, value1, scale);
-        doMath(b2, value2, scale);
-        line.write(b, 0, duration);
-        line2.write(b2, 0, duration);
+    public void compare(final int duration, final int value1, final int value2) throws InterruptedException {
+        int tone = (value1 - minVal) * 75 / scale + 30;
+        Note note = new Note(tone);
+        tone = (value1 - minVal) * 75 / scale + 30;
+        Note note2 = new Note(tone);
+        player.startNote(note);
+        player2.startNote(note2);
+        Thread.sleep(duration);
+        player.stopNote(note);
+        player2.stopNote(note2);
     }
 
-    public void complete(final int duration, int[] elements) {
+    public void complete(final int duration, int[] elements) throws InterruptedException {
         for (final int element : elements) {
-            byte[] b = new byte[16 * 1024 * 2];
-            doMath(b, element, scale);
-            line.write(b, 0, duration);
-        }
-    }
-
-    private static void doMath(byte[] b, int val, double scale) {
-        double exp = ((double) val) / 6d / scale;
-        double f = 440d * Math.pow(2d, exp);
-        for (int i = 0; i < b.length; i++) {
-            double period = (double) 32 * 1024 / f;
-            double angle = 2.0 * Math.PI * i / period;
-            b[i] = (byte) (Math.sin(angle) * 127f);
+            final int tone = (element - minVal) * 75 / scale + 30;
+            Note note = new Note(tone);
+            player.startNote(note);
+            Thread.sleep(duration);
+            player.stopNote(note);
         }
     }
 }
